f08635438540dd2737b1b1e80dfc8c4e4bc55401
victoria12345 <victoria.pelayo@estudiante.uam.es>
2017-10-27 16:51:10 +0200

    añadido game_add_objects

--- game_reader.c
+++ game_reader.c
@@ -1,10 +1,10 @@
-/** 
- * @brief Implementación de la función de carga de espacios. 
- * 
+/**
+ * @brief Implementación de la función de carga de espacios.
+ *
  * @file game_reader.c
  * @author Victoria Pelayo e Ignacio Rabuñal
- * @version 1.0 
- * @date 24-09-2017 
+ * @version 1.0
+ * @date 24-09-2017
  * @copyright GNU Public License
  */
 
@@ -23,16 +23,16 @@ STATUS game_load_spaces(Game* game, char* filename) {
     Id id = NO_ID, north = NO_ID, east = NO_ID, south = NO_ID, west = NO_ID;
     Space* space = NULL;
     STATUS status = OK;
-  
+
     if (!filename) {
         return ERROR;
     }
-  
+
     file = fopen(filename, "r");
     if (file == NULL) {
         return ERROR;
     }
-  
+
     while (fgets(line, WORD_SIZE, file)) {
         if (strncmp("#s:", line, 3) == 0) {
             toks = strtok(line + 3, "|");
@@ -47,7 +47,7 @@ STATUS game_load_spaces(Game* game, char* filename) {
             south = atol(toks);
             toks = strtok(NULL, "|");
             west = atol(toks);
-#ifdef DEBUG 
+#ifdef DEBUG
       printf("Leido: %ld|%s|%ld|%ld|%ld|%ld\n", id, name, north, east, south, west);
 #endif
             space = space_create(id);
@@ -58,17 +58,60 @@ STATUS game_load_spaces(Game* game, char* filename) {
                 space_set_south(space, south);
                 space_set_west(space, west);
                 game_add_space(game, space);
-            } 
+            }
         }
     }
-  
+
     if (ferror(file)) {
         status = ERROR;
     }
-  
+
     fclose(file);
-  
+
     return status;
 }
 
+STATUS game_load_objects(Game* game, char* filename) {
+    FILE* file = NULL;
+    char line[WORD_SIZE] = "";
+    char name[WORD_SIZE] = "";
+    char* toks = NULL;
+    Id id, id_space = NO_ID;
+    Object* object = NULL;
+    STATUS status = OK;
 
+    if (!filename) {
+        return ERROR;
+    }
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        return ERROR;
+    }
+
+    while (fgets(line, WORD_SIZE, file)) {
+        if (strncmp("#o:", line, 3) == 0) {
+            toks = strtok(line + 3, "|");
+            id = atol(toks);
+            toks = strtok(NULL, "|");
+            strcpy(name, toks);
+
+#ifdef DEBUG
+      printf("Leido: %ld|%s\n", id, name);
+#endif
+
+            object = object_create(id, name);
+            if (object != NULL) {
+                game_add_object(game, id);
+            }
+        }
+    }
+
+    if (ferror(file)) {
+        status = ERROR;
+    }
+
+    fclose(file);
+
+    return status;
+}
--- game_reader.h
+++ game_reader.h
@@ -1,10 +1,10 @@
-/** 
+/**
  * @brief Funcionalidad de la carga de espacios
- * 
+ *
  * @file game_reader.h
  * @author Victoria Pelayo e Ignacio Rabuñal
- * @version 1.0 
- * @date 24-09-2017 
+ * @version 1.0
+ * @date 24-09-2017
  * @copyright GNU Public License
  */
 
@@ -13,13 +13,21 @@
 
 #include "game.h"
 
-/** 
+/**
 * @brief carga espacios en el juego a través de un fichero.
 *@param game juego al que vamos a añadir espacios(casillas).
 *@param filename fichero desde el que se va a leer.
-*@return OK si la función se ha realizado correctamente y ERROR si no ha sido así. 
+*@return OK si la función se ha realizado correctamente y ERROR si no ha sido así.
 */
 STATUS game_load_spaces(Game* game, char* filename);
+
+/**
+* @brief carga objetos en el juego a través de un fichero.
+*@param game juego al que vamos a añadir objetos.
+*@param filename fichero desde el que se va a leer.
+*@return OK si la función se ha realizado correctamente y ERROR si no ha sido así.
+*/
+STATUS game_load_objects(Game* game, char* filename);
 
 
 #endif
